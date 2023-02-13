18eaf8de8d5a7a9de1a29c5d5101e8f59230fd0b
fede-schwindt <86686019+fede-schwindt@users.noreply.github.com>
2023-02-13 03:52:34 +0000

    Pending changes exported from your codespace

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,17 @@
 
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyAM7UJdVZo95dz00IKQz_p0uI8Bse5KnZ8',
-    authDomain: 'reboot-22979.firebaseapp.com',
-    projectId: 'reboot-22979',
-    storageBucket: 'reboot-22979.appspot.com',
-    messagingSenderId: '1033966813085',
-    appId: '',
-  },
-  production: false,
-  apiKey: "AIzaSyCADIfYc5d71X-fQNBcHwwB5rizCZ1Ios0",
-  CountryJson: [
+    apiKey: "AIzaSyBnD5uXvKRAFgegdQVrWZaAj3lDQnHOxU0",
+    authDomain: "flytaxi-376601.firebaseapp.com",
+    projectId: "flytaxi-376601",
+    storageBucket: "flytaxi-376601.appspot.com",
+    messagingSenderId: "661849920974",
+    appId: "",
+
+},
+production: false,
+apiKey: "AIzaSyBnD5uXvKRAFgegdQVrWZaAj3lDQnHOxU0",
+CountryJson: [
     {
         "name": "Afghanistan",
         "dialCode": "+93",
